4688626eba3da5e0f7ec3c3099e5a60968f88008
shreya <shreyakesarwani@gmail.com>
2024-07-07 19:13:07 +0530

    Add project files.

---
+++ MovieFusion/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MovieFusion.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
